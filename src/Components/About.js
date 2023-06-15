6c8865195ef029b65d94783473a5e6e8b2585491
stacyyuu <stacyyuu@gmail.com>
2023-06-15 03:49:49 +0000

    added styling

---
+++ src/Components/About.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Container } from '@mui/material';
+
+function About() {
+  return (
+    <Container maxWidth="md">
+    <h1>Hello, my name is Stacy! I am a Full Stack JavaScript Developer.</h1>
+    </Container>
+  );
+}
+
+export default About;
